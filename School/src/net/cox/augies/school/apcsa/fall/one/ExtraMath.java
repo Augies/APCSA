876ab37c7fa86c75946e15d687e3504e2134af59
Augie Shald <augie.shald@mutualofomaha.com>
2019-08-20 09:47:07 -0500

    Added notes to ExtraMath about AP Exam

--- School/src/net/cox/augies/school/apcsa/fall/one/ExtraMath.java
+++ School/src/net/cox/augies/school/apcsa/fall/one/ExtraMath.java
@@ -20,15 +20,34 @@ public class ExtraMath {
 		double h = Math.pow(2, 5);
 		double i = Math.random();
 		double j = Math.sin(Math.PI);
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		System.out.println(d);
-		System.out.println(e);
-		System.out.println(f);
-		System.out.println(g);
-		System.out.println(h);
-		System.out.println(i);
-		System.out.println(j);
+		System.out.println("round 5.1: " + a);
+		System.out.println("floor 5.1: " + b);
+		System.out.println("ceil 5.1: " + c);
+		System.out.println("log10 100: " + d);
+		System.out.println("cos pi: " + e);
+		System.out.println("max of 10 and 100: " + f);
+		System.out.println("min of 10 and 100: " + g);
+		System.out.println("power of 2 to the 5: " + h);
+		System.out.println("random: " + i);
+		System.out.println("sin of pi: " + j);
+
+		// on the AP Exam these show up \/ \/ \/
+		x = Math.pow(Math.log(x) - Math.cos(x + 56.2), Math.min(Math.random(), Math.random()));
+		System.out.println("Big math Operation: " + x);
+
+		int num = -9;
+		double z = Math.pow(Math.abs(num) - Math.floor(x), Math.max(num, Math.floor(x)));
+		System.out.println("z: " + z);
+
+		System.out.println("Random Behavior!");
+		double mys = Math.random();
+		System.out.println("rand: " + mys); // between 0 and 1
+		int mys2 = (int) Math.ceil(Math.random() * 100); // between 1 and 100
+		System.out.println("rand2: " + mys2);
+		int lower = 5;
+		int upper = 7;
+		int random = (int) Math.floor(Math.random() * (upper - lower + 1)) + lower;// general formula for whole numbers
+																					// between upper and lower bound.
+		System.out.println(random);
 	}
 }
