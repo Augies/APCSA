c6c1f1bb65107537dedfcfb7c97f98f76c549bf9
Augie Shald <augie.shald@mutualofomaha.com>
2019-08-20 08:56:04 -0500

    Added More Notes to Operators, as well as utilizing the Math class in ExtraMath

---
+++ School/src/net/cox/augies/school/apcsa/fall/one/ExtraMath.java
@@ -0,0 +1,34 @@
+package net.cox.augies.school.apcsa.fall.one;
+
+/**
+ * 
+ * @author req96090
+ * 
+ *         Exploration of the math class in java
+ */
+public class ExtraMath {
+	public static void main(String[] args) {
+		double x = Math.abs(-5.1);
+		System.out.println("Absolute value of -5.1: " + x);
+		double a = Math.round(5.1);
+		double b = Math.floor(5.1);
+		double c = Math.ceil(5.1);
+		double d = Math.log10(100);
+		double e = Math.cos(Math.PI);
+		double f = Math.max(10, 100);
+		double g = Math.min(10, 100);
+		double h = Math.pow(2, 5);
+		double i = Math.random();
+		double j = Math.sin(Math.PI);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+		System.out.println(j);
+	}
+}
--- School/src/net/cox/augies/school/apcsa/fall/one/Operators.java
+++ School/src/net/cox/augies/school/apcsa/fall/one/Operators.java
@@ -1,7 +1,7 @@
 package net.cox.augies.school.apcsa.fall.one;
 
 /**
- * What can be done with the data type, add, sub, mult, div, etc.
+ * What can be done with the data type, add, subtract, multiply, divide, etc.
  * 
  * @author augies
  */
@@ -10,6 +10,18 @@ public class Operators {
 		int x = 4;
 		int y = 7;
 		double quot = (double) x / (double) y;
-		System.out.println("quot: " + quot);
+		System.out.println("quot of double division: " + quot);
+
+		String name = "augie";
+		// Cannot cast String to an integer
+
+		quot = x / y; // 10/2
+		System.out.println("quot of int division: " + quot);
+
+		int number2 = 7;
+		int number3 = 3;
+		int result = number2 % number3;
+		System.out.println("result of modulus: " + result);
+		// 7/3 = 2 remainder 1. Prints 1
 	}
 }
