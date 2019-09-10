e83f05e7deb248639ef6a18e4f37ead199a07437
augies <augies@cox.net>
2019-09-10 09:07:43 -0500

    Took Basic notes on Two-Dimensional Arrays

---
+++ School/src/net/cox/augies/school/apcsa/fall/one/TwoDArrays.java
@@ -0,0 +1,13 @@
+package net.cox.augies.school.apcsa.fall.one;
+
+public class TwoDArrays {
+	public static void main(String[] args) {
+		int[][] grid = new int[5][5];
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				grid[i][j] = 6;
+			}
+		}
+	}
+
+}
